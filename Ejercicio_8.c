953a0109b9884c333c2161254d9ed1b20582f8f1
CamilaChoque <86363351+CamilaChoque@users.noreply.github.com>
2021-08-10 11:52:06 -0300

    Add files via upload

---
+++ Ejercicio_8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int limite=10, positivo=0,nulos=0,negativos=0;
+ int numero,c;
+printf("Ingrese %d numeros\n",limite);
+ for(c=1;c<=limite;c++)
+ {
+ scanf("%d",&numero);
+ if (numero<0)
+ {
+ negativos++;
+ }
+ else
+ {
+ if(numero==0)
+ {
+ nulos++;
+ }
+ else
+ {
+ if(numero>0)
+ {
+ positivo++;
+ }
+}
+ }
+ }
+printf("\nHay:\n%d numeros negativos\n%d numero positivos\n%d numeros igual a cero",negativos,positivo,nulos);
+return 0;
+}
---
+++ Ejercicio_9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int i,personas=4,menos=0,mas=0,igual=0;
+ float sueldo;
+ printf("Ingrese el sueldo de cada persona registrada:\n");
+for(i=1;i<=personas;i++)
+ {
+ system ("cls");
+ printf("Sueldo de persona %d\n",i);
+ scanf("%f", &sueldo);
+ if (sueldo>2000)
+ {
+ mas++;
+ }
+ else
+ {
+ if(sueldo<2000)
+ {
+ menos++;
+ }
+ else
+ {
+ if(sueldo==2000)
+ {
+ igual++;
+ }
+ }
+ }
+ }
+system("cls");
+printf ("REGISTROS:\n%d personas ganan menos de $2000.\n%d personas ganan mas de $2000.\n%d personas ganan $2000\n",menos,mas,igual);
+ return 0;
+}
